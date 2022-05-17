348f3e649bdaa850b5a28db0b8d2ed6d9cef3b27
MarvynBornemann <marvyn.bornemann@gmail.com>
2022-05-17 16:59:14 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,234 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+void main() => runApp(const VideoPlayerApp());
+
+class VideoPlayerApp extends StatelessWidget {
+  const VideoPlayerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Video Player Demo',
+      home: VideoPlayerScreen(),
+    );
+  }
+}
+
+class VideoPlayerScreen extends StatefulWidget {
+  const VideoPlayerScreen({Key? key}) : super(key: key);
+
+  @override
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  // MQTT
+  String broker = '192.168.1.100';
+  String username = 'LegoDemonstrator';
+  String passwd = 'Lego12Demo34nstr56ator';
+  String clientIdentifier = 'iPad1';
+  String topic = '/idFZy8D9KzFko7db/Video001/cmd';
+
+  late MqttServerClient client;
+  late MqttConnectionState connectionState;
+  late StreamSubscription subscription;
+
+  void _subscribeToTopic(String topic) {
+    if (connectionState == MqttConnectionState.connected) {
+      print('[MQTT client] Subscribing to ${topic.trim()}');
+      client.subscribe(topic, MqttQos.exactlyOnce);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _controller = VideoPlayerController.asset('assets/video/video.mp4');
+
+    // Initialize the controller and store the Future for later use.
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    // Use the controller to loop the video.
+    _controller.setLooping(false);
+
+    _controller.addListener(() {
+      if (_controller.value.position == _controller.value.duration) {
+        setState(() {});
+      }
+    });
+
+    _connect();
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FIWARE'),
+      ),
+      backgroundColor: Colors.black,
+      // Use a FutureBuilder to display a loading spinner while waiting for the
+      // VideoPlayerController to finish initializing.
+      body: _controller.value.isPlaying
+          ? FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // If the VideoPlayerController has finished initialization, use
+                  // the data it provides to limit the aspect ratio of the video.
+                  return AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    // Use the VideoPlayer widget to display the video.
+                    child: VideoPlayer(_controller),
+                  );
+                } else {
+                  // If the VideoPlayerController is still initializing, show a
+                  // loading spinner.
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            )
+          : Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Wrap the play or pause in a call to `setState`. This ensures the
+          // correct icon is shown.
+          setState(() {
+            // If the video is playing, pause it.
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              // If the video is paused, play it.
+              _controller.play();
+            }
+          });
+        },
+        // Display the correct icon depending on the state of the player.
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+
+  void _connect() async {
+    /// First create a client, the client is constructed with a broker name, client identifier
+    /// and port if needed. The client identifier (short ClientId) is an identifier of each MQTT
+    /// client connecting to a MQTT broker. As the word identifier already suggests, it should be unique per broker.
+    /// The broker uses it for identifying the client and the current state of the client. If you don’t need a state
+    /// to be hold by the broker, in MQTT 3.1.1 you can set an empty ClientId, which results in a connection without any state.
+    /// A condition is that clean session connect flag is true, otherwise the connection will be rejected.
+    /// The client identifier can be a maximum length of 23 characters. If a port is not specified the standard port
+    /// of 1883 is used.
+    /// If you want to use websockets rather than TCP see below.
+    ///
+    client = MqttServerClient(broker, clientIdentifier);
+
+    /// A websocket URL must start with ws:// or wss:// or Dart will throw an exception, consult your websocket MQTT broker
+    /// for details.
+    /// To use websockets add the following lines -:
+    /// client.useWebSocket = true;
+    /// client.port = 80;  ( or whatever your WS port is)
+    /// Note do not set the secure flag if you are using wss, the secure flags is for TCP sockets only.
+    /// Set logging on if needed, defaults to off
+    client.logging(on: true);
+
+    /// Set the correct MQTT protocol for testing against mosquito
+    client.setProtocolV311();
+
+    /// If you intend to use a keep alive value in your connect message that is not the default(60s)
+    /// you must set it here
+    client.keepAlivePeriod = 30;
+
+    /// Set auto reconnect
+    client.autoReconnect = true;
+
+    /// Create a connection message to use or use the default one. The default one sets the
+    /// client identifier, any supplied username/password, the default keepalive interval(60s)
+    /// and clean session, an example of a specific one below.
+    final MqttConnectMessage connMess = MqttConnectMessage()
+        .withClientIdentifier(clientIdentifier)
+        .startClean() // Non persistent session for testing
+        .withWillQos(MqttQos.atMostOnce);
+    print('[MQTT client] MQTT client connecting....');
+    client.connectionMessage = connMess;
+
+    /// Connect the client, any errors here are communicated by raising of the appropriate exception. Note
+    /// in some circumstances the broker will just disconnect us, see the spec about this, we however will
+    /// never send malformed messages.
+    try {
+      await client.connect(username, passwd);
+    } catch (e) {
+      print(e);
+      _disconnect();
+    }
+
+    /// Check if we are connected
+    if (client.connectionStatus!.state == MqttConnectionState.connected) {
+      print('[MQTT client] connected');
+      setState(() {
+        connectionState = client.connectionStatus!.state;
+      });
+    } else {
+      print('[MQTT client] ERROR: MQTT client connection failed - '
+          'disconnecting, state is ${client.connectionStatus!.state}');
+      _disconnect();
+    }
+
+    /// The client has a change notifier object(see the Observable class) which we then listen to to get
+    /// notifications of published updates to each subscribed topic.
+    subscription = client.updates!.listen(_onMessage);
+
+    _subscribeToTopic(topic);
+  }
+
+  void _disconnect() {
+    print('[MQTT client] _disconnect()');
+    client.disconnect();
+  }
+
+  void _onMessage(List<MqttReceivedMessage> event) {
+    print(event.length);
+    final MqttPublishMessage recMess = event[0].payload as MqttPublishMessage;
+    final String message =
+        MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+    /// The above may seem a little convoluted for users only interested in the
+    /// payload, some users however may be interested in the received publish message,
+    /// lets not constrain ourselves yet until the package has been in the wild
+    /// for a while.
+    /// The payload is a byte buffer, this will be specific to the topic
+    print('[MQTT client] MQTT message: topic is <${event[0].topic}>, '
+        'payload is <-- ${message} -->');
+    print(client.connectionStatus!.state);
+    print("[MQTT client] message with topic: ${event[0].topic}");
+    print("[MQTT client] message with message: ${message}");
+    if (message.contains('play')) {
+      setState(() {
+        _controller.play();
+      });
+    }
+  }
+}
